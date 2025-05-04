d54ed39b0cd6a29ba43ff4537bd0747d8fd4be70
Andile <andilelethumusa@gmail.com>
2025-05-04 20:01:09 +0200

    Add prodeuct component

--- Frontend/techshop/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ Frontend/techshop/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,26 +1,36 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card'
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { AddProductComponent } from '../add-product/add-product.component';
 @Component({
-  
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [MatCardModule,MatSidenavModule,RouterOutlet,RouterModule,MatButtonModule,MatIconModule,NgClass],
+  imports: [
+    MatCardModule,
+    MatSidenavModule,
+    RouterOutlet,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    NgClass,
+  ],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.scss'
+  styleUrl: './admin-dashboard.component.scss',
 })
 export class AdminDashboardComponent {
-  
-  isOpen = true
+  isOpen = true;
 
-  /**
-   * Toggles the width and content of the side bar
-   */
-  widthToggle(){
-    this.isOpen = !this.isOpen
+  constructor(private dialog: MatDialog) {}
+
+  widthToggle() {
+    this.isOpen = !this.isOpen;
+  }
+  addProduct() {
+    this.dialog.open(AddProductComponent,{width:"600px"})
   }
 }
