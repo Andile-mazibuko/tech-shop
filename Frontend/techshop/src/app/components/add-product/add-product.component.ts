2abc9fb3cfd55b3d646ee4322d572caeca110c23
Andile <andilelethumusa@gmail.com>
2025-05-04 21:50:15 +0200

    Add Product complete

--- Frontend/techshop/src/app/components/add-product/add-product.component.ts
+++ Frontend/techshop/src/app/components/add-product/add-product.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.scss'
+  styleUrl: './add-product.component.scss',
 })
 export class AddProductComponent {
+  constructor(
+    private dialogRef: MatDialogRef<AddProductComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) {}
 
+  closeDialog(){
+    this.dialogRef.close()
+  }
 }
---
+++ Frontend/techshop/src/app/interfaces/models.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    prod_id?: number, 
+    title : string,
+    name : string,
+    description: string,
+    price: number,
+    category: "Computer"|"GPU"|"CPU"|"Keyboard"|"Headset"
+
+}
